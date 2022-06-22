7edc64a12e373aabf57e632aaf3198d7874ba173
teatrologa <li.castrinho@gmail.com>
2022-06-22 13:33:18 -0300

    EmployeeServices finalizado

--- LetsPet854.Domain/Employees/EmployeeServices.cs
+++ LetsPet854.Domain/Employees/EmployeeServices.cs
@@ -3,25 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using LetsPet_Employees.Enums
+using LetsPet_Employees.Enums;
 
 namespace LetsPet_Employees
 {
     public class EmployeeServices
     {
-        public Species specie { get; set; }
-        public TypeService type { get; set; }
-        public SizeAnimal size { get; set; }
+        public List<Species> Species { get; set; }
+        public List<TypeService> Types { get; set; }
+        public List<SizeAnimal> Sizes { get; set; }
         public bool MeetsSpecialNeeds { get; set; }
         public bool MeetsAggressiveAnimal { get; set; }
 
 
-        public EmployeeServices(SizeAnimal size, TypeService type, Species specie, bool meetsSpecialNeeds, bool meetsAggressiveAnimal)
+        public EmployeeServices(List<SizeAnimal> sizes, List<TypeService> types, List<Species> species, bool meetsSpecialNeeds, bool meetsAggressiveAnimal)
         {
 
-            TypeService Type = type;
-            Species Specie = specie;
-            SizeAnimal Size = size;
+            Types = types;
+            Species = species;
+            Sizes = sizes;
             MeetsSpecialNeeds = meetsSpecialNeeds;
             MeetsAggressiveAnimal = meetsAggressiveAnimal;
 
