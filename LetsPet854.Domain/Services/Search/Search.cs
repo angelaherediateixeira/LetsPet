69a1cfcdca339cad894145cfe93c71d936841143
Tainá Maia <taina.cmaia@hotmail.com>
2022-06-23 10:00:32 -0300

    Novas validações, atualização na busca e listagem de informações

--- LetsPet854.Domain/Services/Search/Search.cs
+++ LetsPet854.Domain/Services/Search/Search.cs
@@ -8,79 +8,34 @@ namespace LetsPet854.Domain
 {
     internal class Search
     {
-        public static void TypeSearch(string search)
-        {
-            foreach (var service in Registration.ServicesList)
-            {
-                if (search == service.Type)
-                {
-                    Console.WriteLine($"Nome do serviço: {service.Name}");
-                    Console.Write("Informações do serviço: ");
-                    Console.Write(service.Type);
-                    if (service.GroomingType != null)
-                    {
-                        Console.Write($" {service.GroomingType}");
-                    }
-                    Console.Write($"; Espécie: {service.Species}; ");
-                    Console.Write($"Porte {service.Size}; ");
-                    Console.Write($"Serviço especial: {service.Special}; ");
-                    Console.Write($"Loção: {service.Lotion}; ");
-                    Console.Write($"Tempo: {service.ServiceTime}h; ");
-                    Console.WriteLine($"Preço: {service.Price}.");
-                }
-            }
-
-        }
-
-        public static void SizeOrSpeciesSearch(string search)
-        {
-
-            foreach (var service in Registration.ServicesList)
-            {
-                if (search == service.Species || search == service.Size)
-                {
-                    Console.WriteLine($"Nome do serviço: {service.Name}");
-                    Console.Write("Informações do serviço: ");
-                    Console.Write(service.Type);
-                    if (service.GroomingType != null)
-                    {
-                        Console.Write($" {service.GroomingType}");
-                    }
-                    Console.Write($"; Espécie: {service.Species}; ");
-                    Console.Write($"Porte {service.Size}; ");
-                    Console.Write($"Serviço especial: {service.Special}; ");
-                    Console.Write($"Loção: {service.Lotion}; ");
-                    Console.Write($"Tempo: {service.ServiceTime}h; ");
-                    Console.WriteLine($"Preço: {service.Price}.");
-                }
-            }
-        }
-
         public static void Options()
         {
             string search;
             int input;
             Console.WriteLine("Qual o tipo de pesquisa que deseja realizar?");
             Console.WriteLine("1 - Tipo de Serviços\n2 - Por porte ou espécie");
-            input = int.Parse(Console.ReadLine());
+            while(!int.TryParse(Console.ReadLine(), out input) || (input < 1 || input > 2))
+            {
+                Console.WriteLine("Digite um valor válido:");
+            }
             if (input == 1)
             {
                 Console.WriteLine("Opções de serviço são:\n1 - Banho\n2 - Tosa");
                 input = int.Parse(Console.ReadLine());
                 if (input == 1)
                 {
-                    TypeSearch("Banho");
+                    ShowInfo.ByType("Banho");
                 }
                 else if (input == 2)
                 {
-                    TypeSearch("Tosa");
+                    ShowInfo.ByType("Tosa");
                 }
             }
             else if (input == 2)
             {
                 Console.WriteLine("Digite um porte ou espécie");
                 search = Console.ReadLine();
-                SizeOrSpeciesSearch(search);
+                ShowInfo.BySizeOrSpecies(search);
             }
         }
     }
---
+++ LetsPet854.Domain/Services/Search/ShowInfo.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetsPet854.Domain
+{
+    public class ShowInfo
+    {
+        public static void ByType(string search)
+        {
+            foreach (var service in Registration.ServicesList)
+            {
+                if (search == service.Type)
+                {
+                    Console.WriteLine($"Nome do serviço: {service.Name}");
+                    Console.Write("Informações do serviço: ");
+                    Console.Write(service.Type);
+                    if (service.GroomingType != null)
+                    {
+                        Console.Write($" {service.GroomingType}");
+                    }
+                    Console.Write($"; Espécie: {service.Species}; ");
+                    Console.Write($"Porte {service.Size}; ");
+                    Console.Write($"Serviço especial: {service.Special}; ");
+                    Console.Write($"Loção: {service.Lotion}; ");
+                    Console.Write($"Tempo: {service.ServiceTime}h; ");
+                    Console.WriteLine($"Preço: {service.Price}.");
+                }
+            }
+
+        }
+
+        public static void ByName(string search)
+        {
+            foreach (var service in Registration.ServicesList)
+            {
+                if (search == service.Name)
+                {
+                    Console.WriteLine($"Nome do serviço: {service.Name}");
+                    Console.Write("Informações do serviço: ");
+                    Console.Write(service.Type);
+                    if (service.GroomingType != null)
+                    {
+                        Console.Write($" {service.GroomingType}");
+                    }
+                    Console.Write($"; Espécie: {service.Species}; ");
+                    Console.Write($"Porte {service.Size}; ");
+                    Console.Write($"Serviço especial: {service.Special}; ");
+                    Console.Write($"Loção: {service.Lotion}; ");
+                    Console.Write($"Tempo: {service.ServiceTime}h; ");
+                    Console.WriteLine($"Preço: {service.Price}.");
+                }
+            }
+
+        }
+
+        public static void BySizeOrSpecies(string search)
+        {
+            foreach (var service in Registration.ServicesList)
+            {
+                if (search == service.Species || search == service.Size)
+                {
+                    Console.WriteLine($"Nome do serviço: {service.Name}");
+                    Console.Write("Informações do serviço: ");
+                    Console.Write(service.Type);
+                    if (service.GroomingType != null)
+                    {
+                        Console.Write($" {service.GroomingType}");
+                    }
+                    Console.Write($"; Espécie: {service.Species}; ");
+                    Console.Write($"Porte {service.Size}; ");
+                    Console.Write($"Serviço especial: {service.Special}; ");
+                    Console.Write($"Loção: {service.Lotion}; ");
+                    Console.Write($"Tempo: {service.ServiceTime}h; ");
+                    Console.WriteLine($"Preço: {service.Price}.");
+                }
+            }
+
+        }
+    }
+}
